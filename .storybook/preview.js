3b62c35b2dfb91c95a63addfaeb964a7d497ace9
Tony Hopland Træet <totr@fhi.no>
2022-04-29 09:57:59 +0200

    Added FHITheme to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,15 @@
 import '@folkehelseinstituttet/style/scss/style.scss';
+import { ThemeProvider } from "styled-components";
+import FhiTheme from '../src/components/FhiTheme';
+
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={FhiTheme}>
+      <Story />
+    </ThemeProvider>
+  ),
+];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
