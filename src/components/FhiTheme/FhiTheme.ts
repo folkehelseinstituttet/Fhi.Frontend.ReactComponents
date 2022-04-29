301a48c454d68b13004be1d939df40717d6739de
Tony Hopland Træet <totr@fhi.no>
2022-04-29 08:22:49 +0200

    Theme fix

--- src/components/FhiTheme/FhiTheme.ts
+++ src/components/FhiTheme/FhiTheme.ts
@@ -1,11 +1,3 @@
-let theme;
+import FhiTheme from './FhiThemeVariables.json';
 
-const getTheme = async () => {
-  if (!theme) {
-    const response = await fetch('/FhiThemeVariables.json');
-    theme = await response.json();
-  }
-  return theme;
-};
-
-export default await getTheme();
+export default FhiTheme;
