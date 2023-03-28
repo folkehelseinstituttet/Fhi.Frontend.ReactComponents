e07c58513d2b67b964fd2921ec9d84d819140dce
Tony Hopland Træet <totr@fhi.no>
2023-03-28 11:13:19 +0200

    Fiksed FhiTable onClick

--- src/components/FhiTable/FhiTable.model.ts
+++ src/components/FhiTable/FhiTable.model.ts
@@ -3,6 +3,7 @@ import React from 'react';
 export type TableStructure = {
   key: number,
   link?: string,
+  onClick?: (event: React.MouseEvent<HTMLTableRowElement, MouseEvent>) => void,
   selected?: boolean,
   data: React.ReactNode[]
 };
